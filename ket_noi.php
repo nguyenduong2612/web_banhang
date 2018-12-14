0563f38f2d98e8ffbcbbaccaedd94bbe03f54bc8
Dương Hải Nguyên <36105378+nguyenduong2612@users.noreply.github.com>
2018-12-14 16:29:53 +0700

    Update ket_noi.php

--- ket_noi.php
+++ ket_noi.php
@@ -1,5 +1,5 @@
 <?php
-	$conn =pg_connect("host=localhost dbname=db_banhang user=postgres password=chuonggio1998");
+	$conn =pg_connect("host=localhost dbname=db_banhang user=postgres password=123456");
 ?> 
 <!-- 
 <?php
