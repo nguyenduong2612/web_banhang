785657b12cb4f415dcadaaaf885ee3fba4e9bcd9
Dương Hải Nguyên <36105378+nguyenduong2612@users.noreply.github.com>
2018-12-14 16:29:23 +0700

    Update login.php

--- chuc_nang/login/login.php
+++ chuc_nang/login/login.php
@@ -1,1 +1,1 @@
-<button type="submit" class="btn btn-danger" class="search_button" style="margin-bottom: 3px;position: absolute;right: 26px;top: 24px">Dang nhap</button>
+<button type="submit" class="btn btn-danger" class="search_button" style="margin-bottom: 3px;position: absolute;right: 26px;top: 24px">Đăng nhập</button>
