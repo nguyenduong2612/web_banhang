d2e2f27608adda04fe0b9f6c45b25bc4287d50c1
thanhcong2298 <thanhcong.phung@gmail.com>
2018-12-23 02:27:28 +0700

    Update guide

--- chuc_nang/guide.php
+++ chuc_nang/guide.php
@@ -1,3 +1,75 @@
 <div class="title">
-	<span>Hướng dẫn mua hàng</span>
+	<span style="font-size: 30px">Hướng dẫn mua hàng</span>
+	<div class="text-form">
+		<div class="container">
+			<p>
+				<span style="font-size: 14px">
+					<span style="color: #FFFCFC">
+						<strong>Step 1: Chọn món</strong>
+					</span>
+				</span>
+			</p>	
+			<p>
+				<span style="font-size: 14px">- Chọn món mà bạn thích</span>
+			</p>		
+			<p>
+				<span style="font-size:14px ">
+					<img alt src="hinh_anh/Step_1.PNG" style="border-style: solid;
+					border-width: 1px;
+					float: left;
+					height: 420px;
+					margin-left: 150px;
+					margin-right: 150px;
+					width: 600px">
+				</span>
+			</p>
+			<p>
+				<span style="font-size: 14px">
+					<span style="color: #FFFCFC">
+						<strong>Step 2: Chọn số lượng</strong>
+					</span>
+				</span>
+			</p>
+			<p>
+				<span style="font-size: 14px">- Xác nhận số lượng món và giá tiền</span>
+				<span style="font-size: 14px">- Bạn có thể đặt nhiều món ăn khác nữa!!</span>
+			</p>
+			<p>
+				<span style="font-size:14px ">
+					<img alt src="hinh_anh/step_2.PNG" style="border-style: solid;
+					border-width: 1px;
+					float: left;
+					height: 420px;
+					margin-left: 150px;
+					margin-right: 150px;
+					width: 600px">
+				</span>
+			</p>
+			<p>
+				<p>
+				<span style="font-size: 14px">
+					<span style="color: #FFFCFC">
+						<strong>Step 3: Điền thông tin</strong>
+					</span>
+				</span>
+			</p>
+			<p>
+				<span style="font-size: 14px">- Bạn hãy điền đầy đủ thông tin</span>
+				<span style="font-size: 14px">- Ấn <button>Mua hàng</button> để hoàn thành việc mua hàng trên web</span>
+			</p>
+			<p>
+				<span style="font-size:14px ">
+					<img alt src="hinh_anh/step_3.PNG" style="border-style: solid;
+					border-width: 1px;
+					float: left;
+					height: 420px;
+					margin-left: 150px;
+					margin-right: 150px;
+					width: 600px">
+				</span>
+			</p>
+			</p>
+
+		</div>
+	</div>
 </div>
