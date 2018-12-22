31054c86e9f2e8cea26cd3337e603d758f5ef3d9
thanhcong2298 <thanhcong.phung@gmail.com>
2018-12-23 02:27:16 +0700

    Update contact

--- chuc_nang/contact.php
+++ chuc_nang/contact.php
@@ -1,3 +1,36 @@
 <div class="title">
-	<span>Liên hệ</span>
+	<div class="wrap-contact100">
+
+			<div class="contact100-form-title">
+				<span>Contact Us</span>
+			</div>
+
+			<form class="contact100-form validate-form">
+				<div class="wrap-input10 validate-input">
+					<input id="name" class="input10" type="text" name="name" placeholder="Full name">
+				</div>
+
+
+				<div class="wrap-input100 validate-input">
+					<input id="email" class="input100" type="text" name="email" placeholder="Eg. example@email.com">
+					
+				</div>
+
+
+				<div class="wrap-input100 validate-input">
+					<input id="phone" class="input100" type="text" name="phone" placeholder="Eg. +84 1234 5678">
+					
+				</div>
+
+				<div class="wrap-input100 validate-input">
+					<textarea id="message" class="input100" name="message" placeholder="Your comments..."></textarea>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn">
+						Send Now
+					</button>
+				</div>
+			</form>
+		</div>
 </div>
