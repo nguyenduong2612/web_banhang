d03358c60b34e10d372ccae6e562fa943d2dc260
nguyenduong2612 <nguyenduong261298@gmail.com>
2018-12-25 02:44:32 +0700

    update slideshow

--- chuc_nang/slideshow/slideshow.php
+++ chuc_nang/slideshow/slideshow.php
@@ -1,36 +1,21 @@
-<!-- <?php 
-/*	include("ket_noi.php");	
-	$tv="select hinh,lien_ket from slideshow order by id";
-	$tv_1=pg_query($conn,$tv);
-	while($tv_2=pg_fetch_array($tv_1))
-	{
-		$link_hinh="hinh_anh/slideshow/".$tv_2['hinh'];
-		echo "<img src='".$link_hinh."'>";
-	}*/
-?> -->
-
 <div class="slideshow" id="slideshow">
 		<h1>Trang chủ</h1>
 		<h2>Food lover - Gọi tại nhà ăn thả ga</h2>
 		
 </div>
 
-<!-- <script type="text/javascript" >
-
-	var i_img=0;
-	var div_slideshow=document.getElementById("slideshow");
-	var img_slideshow=div_slideshow.getElementsByTagName("img");
-	for(var i=0;i<img_slideshow.length;i++)
-	{
-		img_slideshow[i].style.display="none";
-	}
-	img_slideshow[0].style.display="block";
-	
-	setInterval(function(){
-		img_slideshow[i_img].style.display="none";
-		i_img=i_img+1;
-		if(i_img>=img_slideshow.length){i_img=0;}
-		img_slideshow[i_img].style.display="block";		
-	},5000);
-
-</script> -->
+<script type="text/javascript">
+    $(document).ready(function() {
+        $(".slideshow").backgroundCycle({
+            imageUrls: [
+                'hinh_anh/slideshow/a_1.png',
+                'hinh_anh/slideshow/a_2.png',
+                'hinh_anh/slideshow/a_3.png',
+                'hinh_anh/slideshow/a_4.png'
+            ],
+            fadeSpeed: 3000,
+            duration: 7000,
+            backgroundSize: SCALING_MODE_COVER
+        });
+    });
+</script>
--- index.php
+++ index.php
@@ -23,6 +23,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script type="text/javascript" src="hieu_ung/background.cycle.js"></script>
 	</head>
 	<body style="font-family: 'Quicksand', sans-serif; width: auto; position: relative;">
 		<div class="header">
