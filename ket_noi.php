0a2b2bdf1fa536cf3265df3a171a2b6b43bb0f25
nguyenduong2612 <nguyenduong261298@gmail.com>
2019-03-27 01:34:52 +0700

    Update ket_noi.php

--- ket_noi.php
+++ ket_noi.php
@@ -1,10 +1,5 @@
 <?php
 
-	$conn =pg_pconnect("host=localhost dbname=db_banhang user=postgres password=123456");
+	$conn =pg_connect("host=ec2-50-17-227-28.compute-1.amazonaws.com dbname=d9muvonefgn9rk user=sncorvhbjcylzi port=5432 password=82cb74dc7ad600a9dc9e2e7ba2eda75aa7f20a2a24300af123cb51c91f425b26");
 
 ?> 
-<!-- 
-<?php
- /*   $conn = mysqli_connect("localhost","root","","db_banhang");
-    mysqli_set_charset($conn,"utf8");*/
-?>  -->
