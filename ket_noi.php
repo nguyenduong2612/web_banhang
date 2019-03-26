1e1f6fd24f17aafbe52d7f736b9c0cedec171194
nguyenduong2612 <nguyenduong261298@gmail.com>
2019-03-27 01:09:38 +0700

    Update ket_noi.php

--- ket_noi.php
+++ ket_noi.php
@@ -1,10 +1,20 @@
-<?php
 
-	$conn =pg_pconnect("host=localhost dbname=db_banhang user=postgres password=123456");
-
-?> 
-<!-- 
 <?php
- /*   $conn = mysqli_connect("localhost","root","","db_banhang");
-    mysqli_set_charset($conn,"utf8");*/
-?>  -->
+	$host = "ec2-50-17-227-28.compute-1.amazonaws.com";
+	$user = "sncorvhbjcylzi";
+	$password = "82cb74dc7ad600a9dc9e2e7ba2eda75aa7f20a2a24300af123cb51c91f425b26";
+	$dbname = "d9muvonefgn9rk";
+	$port = "5432";
+	try{
+	  //Set DSN data source name
+	    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+	  //create a pdo instance
+	  $conn = new PDO($dsn, $user, $password);
+	  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+	  $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+	  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $e) {
+	echo 'Connection failed: ' . $e->getMessage();
+	}
+ ?>
